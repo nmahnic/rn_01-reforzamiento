cc46bd4cc839475b8605bfa9ecb414f9a97060a7
Nicolas Mahnic <nmahnic@pdssa.com.ar>
2022-05-13 00:41:40 -0300

    useSate Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Funciones } from "./typescript/Funciones";
-import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
-import { TiposBasicos } from "./typescript/TiposBasicos";
+// import { Funciones } from "./typescript/Funciones";
+// import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
+// import { TiposBasicos } from "./typescript/TiposBasicos";
+import { Contador } from './Components/Contador';
 
 const App = () => {
   return (
     <div className='mt-2'>
       <h1>Introduccion a TS-REACT</h1>
       <hr/>
-      <TiposBasicos/>
-      <ObjetosLiterales/>
-      <Funciones/>
+      {/* <TiposBasicos/> */}
+      {/* <ObjetosLiterales/> */}
+      {/* <Funciones/> */}
+      <Contador/>
     </div>
   )
 }
